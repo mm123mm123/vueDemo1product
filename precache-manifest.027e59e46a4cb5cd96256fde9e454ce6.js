eb03358694609ee1764740f6f211bca17e588207
mimi <fiymi666@gmail.com>
2020-12-07 16:11:45 +0800

    deploy

---
+++ precache-manifest.027e59e46a4cb5cd96256fde9e454ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2370686e11d550cd5c",
+    "url": "/vueDemo1product/css/app.9400b502.css"
+  },
+  {
+    "revision": "9aa440004b795aa1ad110ec49600b922",
+    "url": "/vueDemo1product/index.html"
+  },
+  {
+    "revision": "9e2370686e11d550cd5c",
+    "url": "/vueDemo1product/js/app.eb804cf7.js"
+  },
+  {
+    "revision": "639c3bc26a90807bdcb6",
+    "url": "/vueDemo1product/js/chunk-vendors.66dd610a.js"
+  },
+  {
+    "revision": "f5729b2892a778e9316927dec7230f82",
+    "url": "/vueDemo1product/manifest.json"
+  },
+  {
+    "revision": "06f2a4f695f84f6bf56f4e1023b69221",
+    "url": "/vueDemo1product/qrqcode.gith.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vueDemo1product/robots.txt"
+  }
+]);
